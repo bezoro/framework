15f1aea02fc4ec1b4fb4746a13cd5a9f81080899
Luiz Otávio Amaral Vaz <bezoro3dartist@outlook.com>
2025-06-03 14:54:04 -0300

    refactor(chess): remove `SetAtSquare` from `PieceModelExtensions`

--- bezoro.core/Chess/Utils/PieceModelExtensions.cs
+++ bezoro.core/Chess/Utils/PieceModelExtensions.cs
@@ -1,12 +1,4 @@
 namespace Bezoro.Core.Chess.Utils
 {
-	public static class PieceModelExtensions
-	{
-		public static void SetAtSquare(this IChessPieceModel pieceToSet, IChessBoardSquareModel targetSquare)
-		{
-			pieceToSet.Square   = targetSquare;
-			pieceToSet.Position = targetSquare.Position;
-			targetSquare.Piece  = pieceToSet;
-		}
-	}
+	public static class PieceModelExtensions { }
 }
