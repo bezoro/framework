a5fdd4efc770f43fdabc37eba21a7de6c0f521b4
Luiz Otávio Amaral Vaz <bezoro3dartist@outlook.com>
2025-06-03 15:01:05 -0300

    feat(chess): add FEN-based piece creation utilities
    - Added `GetPieceTypeFromChar` method for retrieving piece types from FEN characters.
    - Introduced `GetPieceFromChar` for creating piece models based on FEN characters.
    - Added helper method `ToPlayerColor` for deriving player color from character case.

--- bezoro.core/Chess/Utils/ChessUtils.cs
+++ bezoro.core/Chess/Utils/ChessUtils.cs
@@ -2,8 +2,14 @@ using System;
 
 namespace Bezoro.Core.Chess.Utils
 {
-	public class ChessUtils
+	public static class ChessUtils
 	{
+		/// <summary>
+		///     Converts a FEN (Forsyth–Edwards Notation) character to its corresponding chess piece type.
+		/// </summary>
+		/// <param name="fenChar">The FEN character representing a chess piece.</param>
+		/// <returns>The corresponding ChessPieceType.</returns>
+		/// <exception cref="ArgumentException">Thrown when the FEN character is invalid.</exception>
 		public static ChessPieceType GetPieceTypeFromChar(char fenChar)
 		{
 			switch (fenChar)
@@ -17,5 +23,37 @@ namespace Bezoro.Core.Chess.Utils
 				default:  throw new ArgumentException($"Invalid FEN piece character: {fenChar}");
 			}
 		}
+
+		/// <summary>
+		///     Creates a chess piece model from a FEN (Forsyth–Edwards Notation) character.
+		/// </summary>
+		/// <param name="fenChar">The FEN character representing a chess piece.</param>
+		/// <returns>A new instance of IChessPieceModel representing the chess piece.</returns>
+		/// <exception cref="ArgumentException">Thrown when the FEN character is invalid.</exception>
+		public static IChessPieceModel GetPieceFromChar(char fenChar)
+		{
+			var color     = fenChar.ToPlayerColor();
+			var lowerChar = char.ToLower(fenChar);
+
+			return lowerChar switch
+			{
+				'k' => new KingModel(color),
+				'p' => new PawnModel(color),
+				'q' => new QueenModel(color),
+				'r' => new RookModel(color),
+				'b' => new BishopModel(color),
+				'n' => new KnightModel(color),
+				_   => throw new ArgumentException($"Invalid FEN piece character: {fenChar}")
+			};
+		}
+
+		// TODO: Move to proper class
+		/// <summary>
+		///     Converts a character to a PlayerColor based on its case.
+		/// </summary>
+		/// <param name="c">The character to convert.</param>
+		/// <returns>White for uppercase characters, Black for lowercase characters.</returns>
+		private static PlayerColor ToPlayerColor(this char c) =>
+			char.IsUpper(c) ? PlayerColor.White : PlayerColor.Black;
 	}
 }
