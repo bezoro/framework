94c10d820ebc8ec8c8b1106e299c46477e4ec733
Luiz Otávio Amaral Vaz <bezoro3dartist@outlook.com>
2025-06-04 08:48:26 -0300

    refactor(tests): add unit tests for PieceModel class in chess logic

---
+++ bezoro.core.tests/Tests/Chess/PieceModelTests.cs
@@ -0,0 +1,70 @@
+using System.Collections.Generic;
+using Bezoro.Core.Chess;
+using Bezoro.Core.Chess.Interfaces;
+using Bezoro.Core.Chess.Pieces;
+using Moq;
+using NUnit.Framework;
+
+namespace Bezoro.Core.Tests.Chess.Pieces
+{
+	[TestFixture]
+	public class PieceModelTests
+	{
+	#region Test Methods
+
+		[Test]
+		public void GetPseudoLegalMoves_DelegatesToGenerator()
+		{
+			// Arrange
+			var board = Mock.Of<IChessBoardModel>();
+			var moves = new List<Move> { new(new(0, 0), new(0, 1), PlayerColor.White) };
+
+			var genMock = new Mock<IPseudoMoveGenerator>();
+			genMock
+				.Setup(g => g.Generate(board, It.IsAny<PieceModel>()))
+				.Returns(moves);
+
+			var piece = new DummyPiece(PlayerColor.White, genMock.Object);
+
+			// Act
+			var result = piece.GetPseudoLegalMoves(board);
+
+			// Assert
+			Assert.That(result, Is.SameAs(moves)); // returns what generator returned
+			genMock.Verify(g => g.Generate(board, piece), Times.Once);
+		}
+
+		[Test]
+		public void MarkMoved_And_ResetMoved_ToggleFlag()
+		{
+			var piece = new DummyPiece(PlayerColor.White, Mock.Of<IPseudoMoveGenerator>());
+
+			Assert.That(piece.HasMoved, Is.False, "initial");
+			piece.MarkMoved();
+			Assert.That(piece.HasMoved, Is.True, "after MarkMoved");
+			piece.ResetMoved();
+			Assert.That(piece.HasMoved, Is.False, "after ResetMoved");
+		}
+
+		[TestCase(PlayerColor.White, PlayerColor.Black)]
+		[TestCase(PlayerColor.Black, PlayerColor.White)]
+		public void Opposite_ReturnsCorrectColor(PlayerColor input, PlayerColor expected)
+		{
+			var piece = new DummyPiece(input, Mock.Of<IPseudoMoveGenerator>());
+			Assert.That(piece.Opposite, Is.EqualTo(expected));
+		}
+
+	#endregion
+
+	#region Helper Methods/Other Members
+
+		// Concrete helper class just for testing the abstract base
+		private sealed class DummyPiece : PieceModel
+		{
+			public DummyPiece(PlayerColor color, IPseudoMoveGenerator gen)
+				: base(color, gen) { }
+		}
+
+	#endregion
+	}
+}
