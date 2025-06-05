c268ab76fc86265e92fb8975d187fa9efb3d9500
Luiz Otávio Amaral Vaz <bezoro3dartist@outlook.com>
2025-06-05 16:00:00 -0300

    feat(chess): implement queen pseudo-legal move generation
    - Add Generate method to compute all valid queen moves.
    - Include orthogonal and diagonal square calculations for move generation.
    - Introduce EnsureQueenPiece validation to enforce piece type restrictions.

--- bezoro.core/Chess/Pieces/Models/QueenModel.cs
+++ bezoro.core/Chess/Pieces/Models/QueenModel.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using Bezoro.Core.Chess.Abstractions.Interfaces;
+using Bezoro.Core.Chess.Board;
 using Bezoro.Core.Chess.Common.Enums;
+using Bezoro.Core.Chess.Common.Extensions;
 using Bezoro.Core.Chess.Game.Models;
 using Bezoro.Core.Chess.Moves.Models;
 
@@ -16,9 +18,41 @@ namespace Bezoro.Core.Chess.Pieces.Models
 	{
 	#region Interface Implementations
 
-		public IEnumerable<Move> Generate(GameModel game, IChessPieceModel piece) =>
-			throw new NotImplementedException();
+		public IEnumerable<Move> Generate(GameModel game, IChessPieceModel piece)
+		{
+			if (game is null) throw new ArgumentNullException(nameof(game));
+
+			var board = game.Board;
+			var queen = EnsureQueenPiece(board, piece);
+
+			var from = board.GetPosition(queen);
+			if (from is null) yield break;
+
+			// Generate orthogonal moves (like a Rook)
+			foreach (var square in board.GetOrthogonalSquares(from.Value))
+			{
+				var to = new BoardPosition(square.Position.Column, square.Position.Row);
+				yield return new(from.Value, to, queen.Color, queen.GetPieceType());
+			}
+
+			// Generate diagonal moves (like a Bishop)
+			foreach (var square in board.GetDiagonalSquares(from.Value))
+			{
+				var to = new BoardPosition(square.Position.Column, square.Position.Row);
+				yield return new(from.Value, to, queen.Color, queen.GetPieceType());
+			}
+		}
 
 	#endregion
+
+		private static QueenModel EnsureQueenPiece(IChessBoardModel board, IChessPieceModel piece)
+		{
+			if (board is null) throw new ArgumentNullException(nameof(board));
+			if (piece is null) throw new ArgumentNullException(nameof(piece));
+			if (piece is not QueenModel queen)
+				throw new ArgumentException("Generator received a non-queen piece.", nameof(piece));
+
+			return queen;
+		}
 	}
 }
