485c2379b2afa1081bbaad802486966658d6902f
Luiz Otávio Amaral Vaz <bezoro3dartist@outlook.com>
2025-06-05 06:52:13 -0300

    test(chess): add test for pawn legal moves at b2
    - Introduce NUnit test to verify pawn forward moves from b2.
    - Ensure both one-step and two-step moves are returned as legal.
    - Built test logic around StandardChessRules and BoardPosition.

---
+++ bezoro.core.tests/Tests/Chess/StandardChessRulesFilterLegalMovesTests.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using Bezoro.Core.Chess.Pieces;
+using Bezoro.Core.Chess.Rules;
+using NUnit.Framework;
+
+namespace Bezoro.Core.Chess.Tests
+{
+	// TODO: Finish tests
+	[TestFixture]
+	public sealed class StandardChessRules_PawnB2_Tests
+	{
+	#region Test Methods
+
+		[Test]
+		public void FilterLegalMoves_PawnAtB2_ReturnsLegalForwardMoves()
+		{
+			var game     = new GameModel();
+			var pawnB2   = game.Board.GetPieceAt("b2");
+			var squareB2 = game.Board.GetSquare("b2");
+
+			Assert.Multiple(
+				() =>
+				{
+					Assert.That(pawnB2,                Is.Not.Null);
+					Assert.That(pawnB2.GetPieceType(), Is.EqualTo(ChessPieceType.Pawn));
+					Assert.That(pawnB2.Color,          Is.EqualTo(PlayerColor.White));
+				});
+
+			var rules = new StandardChessRules();
+
+			// --- Build moves in the engine’s internal coordinate system ----------
+			var from = squareB2.Position;
+			var toB3 = new BoardPosition(from.Column, from.Row + 1);
+			var toB4 = new BoardPosition(from.Column, from.Row + 2);
+
+			var pseudo = new List<Move>
+			{
+				new(from, toB3, pawnB2.Color),
+				new(from, toB4, pawnB2.Color)
+			};
+
+			// ------------------------- Act ---------------------------------------
+			var filtered = rules.FilterLegalMoves(game, pawnB2, pseudo);
+
+			// ------------------------ Assert -------------------------------------
+			Assert.That(
+				filtered, Is.EquivalentTo(pseudo),
+				"Both one-step and two-step forward pawn moves should be legal.");
+		}
+
+	#endregion
+	}
+}
