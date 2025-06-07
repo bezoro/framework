e4bf43d6c48323bdb5ac28e51d55ebb6b3499093
Luiz Otávio Amaral Vaz <bezoro3dartist@outlook.com>
2025-06-07 18:50:19 -0300

    test(rules): remove obsolete legal move filtering test
    - Removed `FilterLegalMoves_WhenMixedMoves_ReturnsCorrectMoves` test case from `StandardChessRulesTest`.
    - Test no longer aligns with updated rules logic and filtering approach.

--- Bezoro.Chess.UnitTests/Rules/StandardChessRulesTest.cs
+++ Bezoro.Chess.UnitTests/Rules/StandardChessRulesTest.cs
@@ -1,45 +1,7 @@
-using Bezoro.Chess.Abstractions.Interfaces;
-using Bezoro.Chess.Common.Enums;
-using Bezoro.Chess.Common.Extensions;
-using Bezoro.Chess.Common.Helpers;
-using Bezoro.Chess.Game.Models;
-using Bezoro.Chess.Moves.Models;
-using Bezoro.Chess.Pieces.Models;
 using Bezoro.Chess.Rules;
 
 namespace Bezoro.Chess.UnitTests.Rules;
 
 [TestFixture]
 [TestOf(typeof(StandardChessRules))]
-public class StandardChessRulesTest
-{
-#region Test Methods
-
-	[Test]
-	public void FilterLegalMoves_WhenMixedMoves_ReturnsCorrectMoves()
-	{
-		// Arrange
-		var               gameRules   = new StandardChessRules();
-		var pseudoMoves = new List<Move>();
-		var    legalMoves = new List<Move>();
-		IChessPieceModel  piece       = new RookModel(PlayerColor.White);
-		var               game        = new GameModel(FenUtils.EmptyBoard);
-		var               board       = game.Board;
-		var rookToCheck = board.CreatePieceAt("a1",PlayerColor.White,ChessPieceType.Rook);
-		board.CreatePieceAt("a2",PlayerColor.White,ChessPieceType.Pawn);
-		
-		// Act
-		pseudoMoves = rookToCheck.GetPseudoLegalMoves(game).ToList();
-		legalMoves = gameRules.FilterLegalMoves(game, piece, pseudoMoves).ToList();
-
-		// Assert
-		Assert.That(legalMoves, Is.Not.Empty);
-		TestContext.Out.WriteLine($"Number of legal moves: {legalMoves.Count}");
-		foreach (var move in legalMoves)
-		{
-			TestContext.Out.WriteLine(move);
-		}
-	}
-
-#endregion
-}
+public class StandardChessRulesTest { }
