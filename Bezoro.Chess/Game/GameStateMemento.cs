e4df00ac35fb9c35bf29ceaab0bfe16a4c2eea9d
Luiz Otávio Amaral Vaz <bezoro3dartist@outlook.com>
2025-06-09 09:07:27 -0300

    refactor(game): convert GameStateMemento to record
    - Simplified GameStateMemento by using a record instead of a class.
    - Reduced boilerplate code while maintaining immutability.

--- Bezoro.Chess/Game/GameStateMemento.cs
+++ Bezoro.Chess/Game/GameStateMemento.cs
@@ -3,27 +3,10 @@ using Bezoro.Chess.Common.Enums;
 
 namespace Bezoro.Chess.Game
 {
-	public class GameStateMemento
-	{
-		public GameStateMemento(
-			PlayerColor activeColor,
-			CastlingRights castlingRights,
-			IChessBoardSquareModel enPassantTargetSquare,
-			int halfMoveClock,
-			int fullMoveNumber)
-		{
-			ActiveColor           = activeColor;
-			CastlingRights        = castlingRights;
-			EnPassantTargetSquare = enPassantTargetSquare;
-			HalfMoveClock         = halfMoveClock;
-			FullMoveNumber        = fullMoveNumber;
-		}
-
-		public CastlingRights CastlingRights { get; }
-
-		public IChessBoardSquareModel EnPassantTargetSquare { get; }
-		public int                    FullMoveNumber        { get; }
-		public int                    HalfMoveClock         { get; }
-		public PlayerColor            ActiveColor           { get; }
-	}
+	public record GameStateMemento(
+		PlayerColor ActiveColor,
+		CastlingRights CastlingRights,
+		IChessBoardSquareModel EnPassantTargetSquare,
+		int HalfMoveClock,
+		int FullMoveNumber);
 }
