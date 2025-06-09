c87c22cf407feed71f3be527b9600511a8d66f32
Luiz Otávio Amaral Vaz <bezoro3dartist@outlook.com>
2025-06-09 12:11:20 -0300

    test(rules): add unit test for FilterLegalMoves with king safety check
    - Introduced `FilterLegalMoves_WhenWouldLeaveKingInCheck_CorrectlyReturnsFlaggedMove` test.
    - Verified flagged move logic for scenarios leaving the king in check.

--- Bezoro.Chess.UnitTests/Rules/StandardChessRulesUnitTest.cs
+++ Bezoro.Chess.UnitTests/Rules/StandardChessRulesUnitTest.cs
@@ -1,7 +1,33 @@
+using Bezoro.Chess.Common.Enums;
+using Bezoro.Chess.Game.Models;
+using Bezoro.Chess.Moves.Models;
 using Bezoro.Chess.Rules;
 
 namespace Bezoro.Chess.UnitTests.Rules;
 
 [TestFixture]
 [TestOf(typeof(StandardChessRules))]
-public class StandardChessRulesUnitTest { }
+public class StandardChessRulesUnitTest
+{
+#region Test Methods
+
+	[Test]
+	public void FilterLegalMoves_WhenWouldLeaveKingInCheck_CorrectlyReturnsFlaggedMove()
+	{
+		var rules = new StandardChessRules();
+		var game  = new GameModel();
+
+		var move = new Move(
+			new("e2"),
+			new("e4"),
+			PlayerColor.White,
+			ChessPieceType.Pawn,
+			leavesKingInCheck: true);
+
+		var result = rules.FilterLegalMoves(game, [ move ]);
+
+		Assert.That(result.Single().LeavesKingInCheck, Is.True);
+	}
+
+#endregion
+}
