7bd0bc6bf91c95d8457af65b83e40b865cbf613f
Luiz Otávio Amaral Vaz <bezoro3dartist@outlook.com>
2025-06-09 13:49:07 -0300

    test(game): add unit tests for ToFenString method
    - Added `ToFenString_WhenCustomBoard_ReturnsExpectedFenString` test to verify FEN string generation for custom board setups.
    - Added `ToFenString_WhenStandardBoard_ReturnsValidFenString` test to validate FEN string for standard board configuration.

--- Bezoro.Chess.UnitTests/Game/Models/GameModelUnitTests.cs
+++ Bezoro.Chess.UnitTests/Game/Models/GameModelUnitTests.cs
@@ -1,5 +1,6 @@
 using Bezoro.Chess.Common.Enums;
 using Bezoro.Chess.Common.Extensions;
+using Bezoro.Chess.Common.Helpers;
 using Bezoro.Chess.Game.Models;
 using Bezoro.Chess.Moves.Models;
 
@@ -39,6 +40,38 @@ public class GameModelUnitTests
 				Assert.That(moves.Select(m => m.Kind),              Is.All.EqualTo(MoveKind.Normal));
 				Assert.That(moves.Select(m => m.LeavesKingInCheck), Is.All.EqualTo(false));
 			});
+	}
+
+	[Test]
+	public void ToFenString_WhenCustomBoard_ReturnsExpectedFenString()
+	{
+		var game   = new GameModel(FenUtils.EmptyBoard);
+		var board  = game.Board;
+		var pawn   = game.Board.CreatePieceAt("d2", PlayerColor.White, ChessPieceType.Pawn);
+		var knight = game.Board.CreatePieceAt("e4", PlayerColor.White, ChessPieceType.Knight);
+		var bishop = game.Board.CreatePieceAt("f5", PlayerColor.White, ChessPieceType.Bishop);
+		var rook   = game.Board.CreatePieceAt("a1", PlayerColor.Black, ChessPieceType.Rook);
+		game.AddCastlingRights(CastlingRights.WhiteKingSide | CastlingRights.WhiteQueenSide);
+		;
+
+		var fen = game.ToFenString();
+
+		Assert.That(fen, Is.Not.Null);
+		Assert.That(fen, Is.Not.Empty);
+		Assert.That(fen, Is.EquivalentTo("8/8/8/5B2/4N3/8/3P4/r7 w KQ - 0 1"));
+		TestContext.Out.Write($"{fen}");
+	}
+
+	[Test]
+	public void ToFenString_WhenStandardBoard_ReturnsValidFenString()
+	{
+		var game = new GameModel();
+
+		var fen = game.ToFenString();
+		Assert.That(fen, Is.Not.Null);
+		Assert.That(fen, Is.Not.Empty);
+		Assert.That(fen, Is.EquivalentTo(FenUtils.START_FEN));
+		TestContext.Out.Write($"{fen}");
 	}
 
 #endregion
