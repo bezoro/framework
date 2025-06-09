73efaf43fa73428975c1340077e5cf0ae2c811ed
Luiz Otávio Amaral Vaz <bezoro3dartist@outlook.com>
2025-06-09 14:35:30 -0300

    test(move): add unit test for pawn capture scenario
    - Added `Execute_WhenPawnCaptures_RemovesTargetPieceAndMovesPawn` test in `MovePieceCommandUnitTests`.
    - Verified pawn captures correctly remove the target piece from the board and relocate the capturing pawn.

--- Bezoro.Chess.UnitTests/Pieces/Commands/MovePieceCommandUnitTests.cs
+++ Bezoro.Chess.UnitTests/Pieces/Commands/MovePieceCommandUnitTests.cs
@@ -1,5 +1,6 @@
 using Bezoro.Chess.Common.Enums;
 using Bezoro.Chess.Common.Extensions;
+using Bezoro.Chess.Common.Helpers;
 using Bezoro.Chess.Game.Models;
 using Bezoro.Chess.Moves.Models;
 using Bezoro.Chess.Pieces.Commands;
@@ -13,7 +14,23 @@ public class MovePieceCommandUnitTests
 #region Test Methods
 
 	[Test]
-	public void Execute_WhenStartingB2PawnDoublePush_MovesPawnTwoSpaces()
+	public void Execute_WhenPawnCaptures_RemovesTargetPieceAndMovesPawn()
+	{
+		var game        = new GameModel(FenUtils.EmptyBoard);
+		var board       = game.Board;
+		var whitePawn   = board.CreatePieceAt("e4", PlayerColor.White, ChessPieceType.Pawn);
+		var blackPawn   = board.CreatePieceAt("f5", PlayerColor.Black, ChessPieceType.Pawn);
+		var move        = new Move(new("e4"), new("f5"), PlayerColor.White, ChessPieceType.Pawn, MoveKind.Capture);
+		var moveCommand = new MovePieceCommand(move);
+
+		moveCommand.Execute(game);
+
+		Assert.That(board.GetPieceAt("f5"), Is.EqualTo(whitePawn));
+		// Assert.That(board.GetSquareAt("e4").Piece, Is.Null);
+	}
+
+	[Test]
+	public void Execute_WhenStartingB2PawnDoublePush_MovesPawnToB4()
 	{
 		var game        = new GameModel();
 		var board       = game.Board;
