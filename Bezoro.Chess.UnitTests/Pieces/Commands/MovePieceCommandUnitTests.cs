94b8fdade1abce93ac742cb2b6e23ccb6bb4c8bc
Luiz Otávio Amaral Vaz <bezoro3dartist@outlook.com>
2025-06-09 14:22:29 -0300

    test(move): add unit test for pawn double push scenario
    - Added `Execute_WhenStartingB2PawnDoublePush_MovesPawnTwoSpaces` test in `MovePieceCommandUnitTests`.
    - Updated `MovePieceCommand` constructor to public for testing purposes.

--- Bezoro.Chess.UnitTests/Pieces/Commands/MovePieceCommandUnitTests.cs
+++ Bezoro.Chess.UnitTests/Pieces/Commands/MovePieceCommandUnitTests.cs
@@ -1,7 +1,30 @@
+using Bezoro.Chess.Common.Enums;
+using Bezoro.Chess.Common.Extensions;
+using Bezoro.Chess.Game.Models;
+using Bezoro.Chess.Moves.Models;
 using Bezoro.Chess.Pieces.Commands;
 
 namespace Bezoro.Chess.UnitTests.Pieces.Commands;
 
 [TestFixture]
 [TestOf(typeof(MovePieceCommand))]
-public class MovePieceCommandUnitTests { }
+public class MovePieceCommandUnitTests
+{
+#region Test Methods
+
+	[Test]
+	public void Execute_WhenStartingB2PawnDoublePush_MovesPawnTwoSpaces()
+	{
+		var game        = new GameModel();
+		var board       = game.Board;
+		var b2Pawn      = board.GetPieceAt("b2");
+		var move        = new Move(new("b2"), new("b4"), PlayerColor.White, ChessPieceType.Pawn);
+		var moveCommand = new MovePieceCommand(move);
+
+		moveCommand.Execute(game);
+
+		Assert.That(board.GetPieceAt("b4"), Is.EqualTo(b2Pawn));
+	}
+
+#endregion
+}
