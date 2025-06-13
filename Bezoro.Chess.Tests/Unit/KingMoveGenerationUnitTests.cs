ab96c344c37f57a444290922b442ce1535b0f55d
Luiz Otávio Amaral Vaz <bezoro3dartist@outlook.com>
2025-06-13 10:40:17 -0300

    test(move-generation): extract piece-specific move generation tests

---
+++ Bezoro.Chess.Tests/Unit/KingMoveGenerationUnitTests.cs
@@ -0,0 +1,162 @@
+using Bezoro.Chess.ChessLogic;
+using Bezoro.Chess.ChessLogic.Generators;
+using FluentAssertions;
+
+namespace Bezoro.Chess.Tests.Unit;
+
+public class KingMoveGenerationUnitTests
+{
+	[Theory]
+	[InlineData(PieceColor.White)]
+	[InlineData(PieceColor.Black)]
+	public void MoveGenerator_ForKingOnE1_WithBlockingAndCaptures_ShouldGenerateCorrectMoves(PieceColor color)
+	{
+		// Arrange
+		var fromPosition    = new Position(color == PieceColor.White ? "e1" : "e8");
+		var opponentColor   = color.Opposite();
+		var opponentPawnRow = color == PieceColor.White ? fromPosition.Row - 1 : fromPosition.Row + 1;
+
+		var initialBoard = new Piece[8, 8];
+		initialBoard[fromPosition.Row, fromPosition.Col] = new(PieceType.King, color);
+
+		// Friendly pieces (blocking)
+		initialBoard[fromPosition.Row, fromPosition.Col - 1] = new(PieceType.Pawn, color); // d1/d8
+		initialBoard[fromPosition.Row, fromPosition.Col + 1] = new(PieceType.Pawn, color); // f1/f8
+
+		// Enemy pieces (capturable)
+		initialBoard[opponentPawnRow, fromPosition.Col - 1] = new(PieceType.Pawn, opponentColor); // d2/d7
+		initialBoard[opponentPawnRow, fromPosition.Col]     = new(PieceType.Pawn, opponentColor); // e2/e7
+		initialBoard[opponentPawnRow, fromPosition.Col + 1] = new(PieceType.Pawn, opponentColor); // f2/f7
+
+		var gameState = new GameState
+		{
+			PiecePositions = initialBoard,
+			ActiveColor    = color
+		};
+
+		// Act
+		var moves = MoveGenerator.GeneratePieceMoves(fromPosition, gameState).ToList();
+
+		// Assert
+		// King can move to d2, e2, f2 (or d7, e7, f7 for black)
+		moves.Should().HaveCount(3);
+		moves.Select(m => m.To.ToString()).Should()
+			 .Contain(new Position(opponentPawnRow, fromPosition.Col - 1).ToString());
+
+		moves.Select(m => m.To.ToString()).Should().Contain(new Position(opponentPawnRow, fromPosition.Col).ToString());
+		moves.Select(m => m.To.ToString()).Should()
+			 .Contain(new Position(opponentPawnRow, fromPosition.Col + 1).ToString());
+	}
+
+	[Theory]
+	[InlineData(PieceColor.White)]
+	[InlineData(PieceColor.Black)]
+	public void MoveGenerator_ForKingWithBlockedPath_ShouldNotGenerateCastlingMoves(PieceColor color)
+	{
+		// Arrange
+		var isWhite      = color == PieceColor.White;
+		var kingRow      = isWhite ? 7 : 0;
+		var fromPosition = new Position(kingRow, 4);
+
+		var initialBoard = new Piece[8, 8];
+		initialBoard[kingRow, 4] = new(PieceType.King, color);
+		initialBoard[kingRow, 0] = new(PieceType.Rook, color);
+		initialBoard[kingRow, 7] = new(PieceType.Rook, color);
+		// Add blocking pieces
+		initialBoard[kingRow, 1] = new(PieceType.Knight, color); // Queenside
+		initialBoard[kingRow, 6] = new(PieceType.Bishop, color); // Kingside
+
+		var gameState = new GameState
+		{
+			PiecePositions = initialBoard,
+			ActiveColor    = color,
+			Castling       = isWhite ? CastlingRights.WhiteBoth : CastlingRights.BlackBoth
+		};
+
+		// Act
+		var moves = MoveGenerator.GeneratePieceMoves(fromPosition, gameState).ToList();
+
+		// Assert
+		moves.Should().NotContain(m => m.Type == MoveType.CastleKingside);
+		moves.Should().NotContain(m => m.Type == MoveType.CastleQueenside);
+	}
+
+	[Theory]
+	[InlineData(PieceColor.White)]
+	[InlineData(PieceColor.Black)]
+	public void MoveGenerator_ForKingWithClearPath_ShouldGenerateCastlingMoves(PieceColor color)
+	{
+		// Arrange
+		var isWhite      = color == PieceColor.White;
+		var kingRow      = isWhite ? 7 : 0;
+		var fromPosition = new Position(kingRow, 4);
+
+		var initialBoard = new Piece[8, 8];
+		initialBoard[kingRow, 4] = new(PieceType.King, color);
+		initialBoard[kingRow, 0] = new(PieceType.Rook, color);
+		initialBoard[kingRow, 7] = new(PieceType.Rook, color);
+
+		var gameState = new GameState
+		{
+			PiecePositions = initialBoard,
+			ActiveColor    = color,
+			Castling       = isWhite ? CastlingRights.WhiteBoth : CastlingRights.BlackBoth
+		};
+
+		// Act
+		var moves = MoveGenerator.GeneratePieceMoves(fromPosition, gameState).ToList();
+
+		// Assert
+		moves.Should().Contain(m => m.Type == MoveType.CastleKingside);
+		moves.Should().Contain(m => m.Type == MoveType.CastleQueenside);
+	}
+
+	[Theory]
+	[InlineData(PieceColor.White)]
+	[InlineData(PieceColor.Black)]
+	public void MoveGenerator_ForLoneKingOnD4_ShouldGenerate8Moves(PieceColor color)
+	{
+		// Arrange
+		var fromPosition = new Position("d4");
+		var initialBoard = new Piece[8, 8];
+		initialBoard[fromPosition.Row, fromPosition.Col] = new(PieceType.King, color);
+
+		var gameState = new GameState
+		{
+			PiecePositions = initialBoard,
+			ActiveColor    = color
+		};
+
+		// Act
+		var moves = MoveGenerator.GeneratePieceMoves(fromPosition, gameState).ToList();
+
+		// Assert
+		moves.Should().HaveCount(8);
+		moves.Select(m => m.To.ToString()).Should().Contain("d5");
+		moves.Select(m => m.To.ToString()).Should().Contain("c5");
+		moves.Select(m => m.To.ToString()).Should().Contain("e5");
+		moves.Select(m => m.To.ToString()).Should().Contain("d3");
+		moves.Select(m => m.To.ToString()).Should().Contain("c3");
+		moves.Select(m => m.To.ToString()).Should().Contain("e3");
+		moves.Select(m => m.To.ToString()).Should().Contain("c4");
+		moves.Select(m => m.To.ToString()).Should().Contain("e4");
+	}
+
+	[Theory]
+	[InlineData(PieceColor.White)]
+	[InlineData(PieceColor.Black)]
+	public void MoveGenerator_ForStandardStartingKing_ShouldGenerateZeroMoves(PieceColor color)
+	{
+		// Arrange
+		var isWhite = color == PieceColor.White;
+		// In a standard game setup, the king is blocked by its own pieces.
+		var fromPosition = new Position(isWhite ? "e1" : "e8");
+		var gameState    = BoardSetup.CreateStandardGame() with { ActiveColor = color };
+
+		// Act
+		var moves = MoveGenerator.GeneratePieceMoves(fromPosition, gameState).ToList();
+
+		// Assert
+		moves.Should().HaveCount(0);
+	}
+}
---
+++ Bezoro.Chess.Tests/Unit/QueenMoveGenerationUnitTests.cs
@@ -0,0 +1,113 @@
+using Bezoro.Chess.ChessLogic;
+using Bezoro.Chess.ChessLogic.Generators;
+using FluentAssertions;
+
+namespace Bezoro.Chess.Tests.Unit;
+
+public class QueenMoveGenerationUnitTests
+{
+	[Theory]
+	[InlineData(PieceColor.White)]
+	[InlineData(PieceColor.Black)]
+	public void MoveGenerator_ForLoneQueenOnD4_ShouldGenerate27Moves(PieceColor color)
+	{
+		// Arrange
+		var fromPosition = new Position("d4");
+		var initialBoard = new Piece[8, 8];
+		initialBoard[fromPosition.Row, fromPosition.Col] = new(PieceType.Queen, color);
+
+		var gameState = new GameState
+		{
+			PiecePositions = initialBoard,
+			ActiveColor    = color
+		};
+
+		// Act
+		var moves = MoveGenerator.GeneratePieceMoves(fromPosition, gameState).ToList();
+
+		// Assert
+		// 14 rook moves + 13 bishop moves = 27
+		moves.Should().HaveCount(27);
+		// Rook moves
+		moves.Select(m => m.To.ToString()).Should().Contain("d1");
+		moves.Select(m => m.To.ToString()).Should().Contain("d8");
+		moves.Select(m => m.To.ToString()).Should().Contain("a4");
+		moves.Select(m => m.To.ToString()).Should().Contain("h4");
+		// Bishop moves
+		moves.Select(m => m.To.ToString()).Should().Contain("a1");
+		moves.Select(m => m.To.ToString()).Should().Contain("h8");
+		moves.Select(m => m.To.ToString()).Should().Contain("a7");
+		moves.Select(m => m.To.ToString()).Should().Contain("g1");
+	}
+
+	[Theory]
+	[InlineData(PieceColor.White)]
+	[InlineData(PieceColor.Black)]
+	public void MoveGenerator_ForQueenOnD4_WithBlockingAndCaptures_ShouldGenerateCorrectMoves(PieceColor color)
+	{
+		// Arrange
+		var fromPosition  = new Position("d4");
+		var opponentColor = color.Opposite();
+
+		var initialBoard = new Piece[8, 8];
+		initialBoard[fromPosition.Row, fromPosition.Col] = new(PieceType.Queen, color);
+
+		// Add pieces to block and be captured
+		// Friendly (blocking)
+		initialBoard[new Position("d6").Row, new Position("d6").Col] = new(PieceType.Pawn, color); // Up
+		initialBoard[new Position("b4").Row, new Position("b4").Col] = new(PieceType.Pawn, color); // Left
+		initialBoard[new Position("b6").Row, new Position("b6").Col] = new(PieceType.Pawn, color); // Up-Left
+		initialBoard[new Position("f2").Row, new Position("f2").Col] = new(PieceType.Pawn, color); // Down-Right
+		// Enemy (capturable)
+		initialBoard[new Position("d2").Row, new Position("d2").Col] = new(PieceType.Pawn, opponentColor); // Down
+		initialBoard[new Position("g4").Row, new Position("g4").Col] = new(PieceType.Pawn, opponentColor); // Right
+		initialBoard[new Position("f6").Row, new Position("f6").Col] = new(PieceType.Pawn, opponentColor); // Up-Right
+		initialBoard[new Position("b2").Row, new Position("b2").Col] = new(PieceType.Pawn, opponentColor); // Down-Left
+
+		var gameState = new GameState
+		{
+			PiecePositions = initialBoard,
+			ActiveColor    = color
+		};
+
+		// Act
+		var moves = MoveGenerator.GeneratePieceMoves(fromPosition, gameState).ToList();
+
+		// Assert
+		// Expected moves:
+		// Up: d5 -> 1
+		// Down: d3, d2(C) -> 2
+		// Left: c4 -> 1
+		// Right: e4, f4, g4(C) -> 3
+		// Up-Left: c5 -> 1
+		// Up-Right: e5, f6(C) -> 2
+		// Down-Left: c3, b2(C) -> 2
+		// Down-Right: e3 -> 1
+		// Total: 13 moves
+		moves.Should().HaveCount(13);
+		moves.Select(m => m.To.ToString()).Should().Contain("d2");    // Capture
+		moves.Select(m => m.To.ToString()).Should().Contain("g4");    // Capture
+		moves.Select(m => m.To.ToString()).Should().Contain("f6");    // Capture
+		moves.Select(m => m.To.ToString()).Should().Contain("b2");    // Capture
+		moves.Select(m => m.To.ToString()).Should().NotContain("d1"); // Blocked by capture
+		moves.Select(m => m.To.ToString()).Should().NotContain("a4"); // Blocked by friendly
+	}
+
+	[Theory]
+	[InlineData(PieceColor.White)]
+	[InlineData(PieceColor.Black)]
+	public void MoveGenerator_ForStandardStartingQueen_ShouldGenerateZeroMoves(PieceColor color)
+	{
+		// Arrange
+		var isWhite = color == PieceColor.White;
+		// In a standard game setup, the queen is blocked by pawns.
+		var fromPosition = new Position(isWhite ? "d1" : "d8");
+		var gameState    = BoardSetup.CreateStandardGame() with { ActiveColor = color };
+
+		// Act
+		var moves = MoveGenerator.GeneratePieceMoves(fromPosition, gameState).ToList();
+
+		// Assert
+		moves.Should().HaveCount(0);
+	}
+}
