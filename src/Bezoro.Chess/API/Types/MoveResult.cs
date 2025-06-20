18128216234e60ffee746179ac04f891689b6279
Luiz Otávio Amaral Vaz <bezoro3dartist@outlook.com>
2025-06-20 12:23:32 -0300

    refactor(chess): move MoveViewModel to its own file for better modularity

--- src/Bezoro.Chess/API/Types/MoveResult.cs
+++ src/Bezoro.Chess/API/Types/MoveResult.cs
@@ -1,4 +1,3 @@
-using Bezoro.Chess.API.Shared.Enums;
 using Bezoro.Chess.Domain.Types.Structs;
 using Bezoro.Core;
 
@@ -39,26 +38,5 @@ namespace Bezoro.Chess.API.Types
 		/// </summary>
 		internal static Result<MoveResult> Failed(FailureReason reason) =>
 			Result<MoveResult>.Failed(reason);
-	}
-
-	public struct MoveViewModel
-	{
-		internal MoveViewModel(in Move move)
-		{
-			Type               = move.Type.ToAPI();
-			CapturedPieceType  = move.CapturedPiece.Type.ToAPI();
-			MovingPieceType    = move.Piece.Type.ToAPI();
-			From               = move.From.Coordinate;
-			To                 = move.To.Coordinate;
-			PromotionPieceType = move.PromotionPieceType.ToAPI();
-		}
-
-		public ChessSquareCoordinate From { get; }
-		public ChessSquareCoordinate To   { get; }
-
-		public MoveType      Type               { get; }
-		public PieceType     CapturedPieceType  { get; }
-		public PieceType     MovingPieceType    { get; }
-		public PromotionType PromotionPieceType { get; }
 	}
 }
