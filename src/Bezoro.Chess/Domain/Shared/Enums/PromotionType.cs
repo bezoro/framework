f4ea4278696298139e352ae25671d1bf0606e1f3
Luiz Otávio Amaral Vaz <bezoro3dartist@outlook.com>
2025-06-20 17:37:57 -0300

    refactor(chess): format PromotionType enum for improved readability

--- src/Bezoro.Chess/Domain/Shared/Enums/PromotionType.cs
+++ src/Bezoro.Chess/Domain/Shared/Enums/PromotionType.cs
@@ -3,6 +3,9 @@ namespace Bezoro.Chess.Domain.Shared.Enums
 	internal enum PromotionType : byte
 	{
 		None,
-		Queen = PieceType.Queen, Rook = PieceType.Rook, Bishop = PieceType.Bishop, Knight = PieceType.Knight
+		Queen  = PieceType.Queen,
+		Rook   = PieceType.Rook,
+		Bishop = PieceType.Bishop,
+		Knight = PieceType.Knight
 	}
 }
