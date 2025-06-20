6c443bc58b67f741fc0fafb531ba046b1e329592
Luiz Otávio Amaral Vaz <bezoro3dartist@outlook.com>
2025-06-20 16:34:55 -0300

    feat(chess): introduce ChessEngine for move generation and application
    - Add `ChessEngine` record to handle move generation and application logic.
    - Implement `GetCurrentLegalMoves` to fetch valid moves from the current game state.
    - Add `TryApplyMove` for validating and applying moves to update game state.
    - Introduce `GameStateViewModel` for representing the board state in the API.

---
+++ src/Bezoro.Chess/API/Engine/ChessEngine.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using Bezoro.Chess.API.Extensions;
+using Bezoro.Chess.API.ViewModels;
+using Bezoro.Chess.Domain.Extensions;
+using Bezoro.Chess.Domain.Functions.Moves;
+using Bezoro.Chess.Domain.Types.Records;
+using Bezoro.Chess.Domain.Types.Structs;
+using Bezoro.Core;
+using MoveType = Bezoro.Chess.API.Shared.Enums.MoveType;
+
+namespace Bezoro.Chess.API.Engine
+{
+	/// <summary>
+	///     Entrypoint to the system
+	/// </summary>
+	public record ChessEngine
+	{
+		private GameState _state;
+
+		public Result<MoveViewModel[]> GetCurrentLegalMoves()
+		{
+			IEnumerable<Move> moves = MoveGenerator.GenerateMoves(_state);
+
+			var viewModels = new List<MoveViewModel>();
+			foreach (Move move in moves)
+			{
+				viewModels.Add(new MoveViewModel(move));
+			}
+
+			Result<MoveViewModel[]> result = Result<MoveViewModel[]>.Succeeded(viewModels.ToArray());
+			return result;
+		}
+
+		public Result<MoveViewModel> TryApplyMove(MoveViewModel moveViewModel)
+		{
+			Move          move           = default;
+			Position      from           = moveViewModel.From.ToDomain();
+			Position      to             = moveViewModel.To.ToDomain();
+			MoveType      type           = moveViewModel.Type;
+			Piece         piece          = moveViewModel.Piece.ToDomain();
+			Piece         capturedPiece  = moveViewModel.CapturedPiece.ToDomain();
+			PromotionType promotionPiece = moveViewModel.PromotionPieceType.ToDomain();
+
+			switch (type)
+			{
+				case MoveType.None:
+					break;
+				case MoveType.Normal:
+					move = Move.CreateNormal(from, to, piece);
+					break;
+				case MoveType.Capture:
+					move = Move.CreateCapture(from, to, piece, capturedPiece);
+					break;
+				case MoveType.CastleKingside:
+					move = Move.CreateCastleKingside(from, to, piece);
+					break;
+				case MoveType.CastleQueenside:
+					move = Move.CreateCastleQueenside(from, to, piece);
+					break;
+				case MoveType.EnPassant:
+					move = Move.CreateEnPassant(from, to, piece, capturedPiece);
+					break;
+				case MoveType.QuietPromotion:
+					move = Move.CreateQuietPromotion(from, to, piece, promotionPiece);
+					break;
+				case MoveType.CapturePromotion:
+					move = Move.CreateCapturePromotion(from, to, piece, capturedPiece, promotionPiece);
+					break;
+				default:
+					throw new ArgumentOutOfRangeException();
+			}
+
+			GameState newState = MoveExecution.ExecuteMove(_state, move);
+			_state = newState;
+			return Result<MoveViewModel>.Succeeded(moveViewModel);
+		}
+	}
+
+	public readonly struct GameStateViewModel
+	{
+		private readonly ImmutableArray<PieceViewModel> _board;
+
+		public GameStateViewModel(ImmutableArray<PieceViewModel> board)
+		{
+			_board = board;
+		}
+
+		public ReadOnlySpan<PieceViewModel> Board => _board.AsSpan();
+	}
+}
