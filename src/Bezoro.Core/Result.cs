9cb6ce93233c7bf94223cce4d2dc2e16fac1d8e9
Luiz Otávio Amaral Vaz <bezoro3dartist@outlook.com>
2025-06-20 16:55:44 -0300

    feat(core): add static helpers for Result creation
    - Introduce `Failed` and `Succeeded` static methods in `Result` class.
    - Simplify creation of `Result<T>` instances for failure and success scenarios.

--- src/Bezoro.Core/Result.cs
+++ src/Bezoro.Core/Result.cs
@@ -10,6 +10,12 @@ namespace Bezoro.Core
 		InvalidMove
 	}
 
+	public static class Result
+	{
+		public static Result<T> Failed<T>(FailureReason reason) => Result<T>.Failed(reason);
+		public static Result<T> Succeeded<T>(in T data) => Result<T>.Succeeded(data);
+	}
+	
 	/// <summary>
 	///     Represents a result that can either be successful with data of type T or failed with a reason.
 	/// </summary>
