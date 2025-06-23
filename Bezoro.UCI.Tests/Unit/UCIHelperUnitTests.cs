6fbcbac9fe5a6bae859737cec42b229c9787c075
Luiz Otávio Amaral Vaz <bezoro3dartist@outlook.com>
2025-06-23 13:19:29 -0300

    test(uci): add unit tests for UCIHelper methods
    - Add tests for `GetPlayerColorFromFen` to validate player color extraction from FEN strings.
    - Add tests for `IsValidAlgebraicNotation` to verify correct handling of valid chess square notations.

---
+++ Bezoro.UCI.Tests/Unit/UCIHelperUnitTests.cs
@@ -0,0 +1,35 @@
+using JetBrains.Annotations;
+
+namespace Bezoro.UCI.Tests.Unit;
+
+[TestSubject(typeof(UCIHelper))]
+public class UCIHelperUnitTests
+{
+	[Theory]
+	[InlineData("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",    'w')]
+	[InlineData("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1", 'b')]
+	[InlineData("8/2k5/8/8/8/8/3K4/8 w - - 0 1",                               'w')]
+	[InlineData("8/2k5/8/8/8/8/3K4/8 b - - 0 1",                               'b')]
+	public void GetPlayerColorFromFen_WhenValidFen_ReturnsCorrectColor(string fen, char expectedColor)
+	{
+		// Act
+		char? result = UCIHelper.GetPlayerColorFromFen(fen);
+
+		// Assert
+		Assert.Equal(expectedColor, result);
+	}
+
+	[Theory]
+	[InlineData("a1")]
+	[InlineData("h8")]
+	[InlineData("e4")]
+	[InlineData("g5")]
+	public void IsValidAlgebraicNotation_WhenValidNotation_ReturnsTrue(string validSquare)
+	{
+		// Act
+		bool result = UCIHelper.IsValidAlgebraicNotation(validSquare);
+
+		// Assert
+		Assert.True(result);
+	}
+}
