a2793328279ea6c61eca700a26575a8ed8a0e36d
Luiz Otávio Amaral Vaz <bezoro3dartist@outlook.com>
2025-06-25 19:18:12 -0300

    refactor(uci): add new properties and rename for move representation
    - Rename `IsNormalMove` to `IsNormal` for brevity.
    - Add `From`, `To`, and `Move` properties for detailed algebraic move notations.
    - Implement `ToString` to return the move string representation.

--- src/Bezoro.UCI/Types/MoveClassification.cs
+++ src/Bezoro.UCI/Types/MoveClassification.cs
@@ -23,31 +23,37 @@ namespace Bezoro.UCI.Types
 		/// <summary>
 		///     True if the move is a quiet, non-special move.
 		/// </summary>
-		public bool IsNormalMove => !IsCapture && !IsCastling && !IsPromotion;
-
+		public bool IsNormal => !IsCapture && !IsCastling && !IsPromotion;
+		/// <summary>
+		///     The starting square of the move in algebraic notation (e.g., "e2").
+		/// </summary>
+		public string From => Move[..2];
+		/// <summary>
+		///     The move in long algebraic notation (e.g., "e2e4", "e1g1", "a7a8q").
+		/// </summary>
+		public string Move { get; }
+		/// <summary>
+		///     The destination square of the move in algebraic notation (e.g., "e4").
+		/// </summary>
+		public string To => Move.Substring(2, 2);
 		/// <summary>
 		///     True if the move is a standard capture or an en passant capture.
 		/// </summary>
 		public bool IsCapture { get; init; }
-
 		/// <summary>
 		///     True if the move is a castling move (e.g., "e1g1" or "e8c8").
 		/// </summary>
 		public bool IsCastling { get; init; }
-
 		/// <summary>
 		///     True if the move is an en passant capture.
 		/// </summary>
 		public bool IsEnPassant { get; init; }
-
 		/// <summary>
 		///     True if the move is a pawn promotion.
 		/// </summary>
 		public bool IsPromotion { get; init; }
 
-		/// <summary>
-		///     The move in long algebraic notation (e.g., "e2e4", "e1g1", "a7a8q").
-		/// </summary>
-		public string Move { get; init; }
+		public override string ToString() =>
+			Move;
 	}
 }
