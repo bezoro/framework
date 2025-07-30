9f0bcaac36889ffbb1ecb7091350d7de39eb2f25
Luiz Otávio Amaral Vaz <bezoro3dartist@outlook.com>
2025-07-30 16:49:00 -0300

    feat(core): add IntExtensions utilities
    - Introduce extension methods for common integer operations like `IsEven`, `IsOdd`, `Clamp`, and more.
    - Enhance code reusability and clarity in handling integers.

---
+++ src/Bezoro.Core/Common/Extensions/IntExtensions.cs
@@ -0,0 +1,16 @@
+namespace Bezoro.Core.Common.Extensions;
+
+public static class IntExtensions
+{
+	public static bool IsEven(this int value) => value % 2 == 0;
+	public static bool IsNegative(this int value) => value < 0;
+	public static bool IsOdd(this int value) => value % 2 != 0;
+	public static bool IsPositive(this int value) => value > 0;
+	public static bool IsZero(this int value) => value == 0;
+	public static int  Abs(this int value) => value < 0 ? -value : value;
+	public static int  Clamp(this int value, int min, int max) => value < min ? min : value > max ? max : value;
+	public static int  ClampMax(this int value, int max) => value > max ? max : value;
+	public static int  ClampMin(this int value, int min) => value < min ? min : value;
+	public static int  RoundToNearest(this int value, int nearest) => (value + nearest / 2) / nearest * nearest;
+	public static int  Sign(this int value) => value < 0 ? -1 : 1;
+}
