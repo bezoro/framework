36dfd660ddf337307ba62a074e978ecb910cdb5d
Luiz Otávio Amaral Vaz <bezoro3dartist@outlook.com>
2025-08-04 10:21:57 -0300

    feat(core): add SafeInvoke extension for Action
    - Introduce `SafeInvoke` method to safely invoke delegates with exception handling.
    - Log exceptions using `Logger.LogException` for improved debugging.

---
+++ src/Bezoro.Core/Common/Extensions/ActionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Bezoro.Core.Common.Extensions;
+
+public static class ActionExtensions
+{
+	public static void SafeInvoke(Action? handler)
+	{
+		if (handler == null) return;
+
+		foreach (var d in handler.GetInvocationList())
+			try
+			{
+				((Action)d)();
+			}
+			catch (Exception ex)
+			{
+				Logger.LogException(ex);
+			}
+	}
+}
