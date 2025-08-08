c956f0ab2f3b6b5681a865b76e5de8e0cdde209f
Luiz Otávio Amaral Vaz <bezoro3dartist@outlook.com>
2025-08-08 10:15:48 -0300

    feat(core): add CallerArgumentExpressionAttribute for pre-NET6_0 compatibility
    - Introduce `CallerArgumentExpressionAttribute` to provide backward compatibility for pre-.NET 6.0 environments.

---
+++ src/Bezoro.Core/CallerArgumentExpressionAttribute.cs
@@ -0,0 +1,11 @@
+#if !NET6_0_OR_GREATER
+// ReSharper disable once CheckNamespace
+namespace System.Runtime.CompilerServices
+{
+	[AttributeUsage(AttributeTargets.Parameter)]
+	internal sealed class CallerArgumentExpressionAttribute(string parameterName) : Attribute
+	{
+		public string ParameterName { get; } = parameterName;
+	}
+}
+#endif
