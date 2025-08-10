e1ee06f21ffa5c6c6bec52d0f7924c4d221fa793
Luiz Otávio Amaral Vaz <bezoro3dartist@outlook.com>
2025-08-10 14:19:31 -0300

    remove(core): delete unused ArrayExtensions class
    - Remove `ArrayExtensions` as it is no longer utilized in the codebase.

--- src/Bezoro.Core/Common/Extensions/ArrayExtensions.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Bezoro.Core.Common.Extensions;
-
-/// <summary>
-///     Helpers for quick null/empty checks on arrays.
-///     A <c>null</c> array is treated as <em>not</em> empty.
-/// </summary>
-public static class ArrayExtensions { }
