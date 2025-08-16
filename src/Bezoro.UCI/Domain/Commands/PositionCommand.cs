1609f863c8170560b823253ac309d5d75f5cf92a
Luiz Otávio Amaral Vaz <bezoro3dartist@outlook.com>
2025-08-16 14:54:39 -0300

    UCI: add PositionCommand and SearchCommand domain commands
    - Introduce PositionCommand for building 'position' commands from FEN and moves
    - Introduce fluent SearchCommand for building 'go' commands with time/depth/searchmoves/ponder/etc.
    - Encapsulate command string construction and normalization

---
+++ src/Bezoro.UCI/Domain/Commands/PositionCommand.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using Bezoro.UCI.API.Types;
+using Bezoro.UCI.Domain.Common.Constants;
+
+namespace Bezoro.UCI.Domain.Commands;
+
+internal record struct PositionCommand
+{
+	public PositionCommand(string fen, IEnumerable<string>? moves = null)
+	{
+		var parsedFen = Fen.Parse(fen);
+		Fen   = parsedFen;
+		Moves = moves;
+	}
+
+	public Fen                  Fen   { get; set; }
+	public IEnumerable<string>? Moves { get; set; }
+
+	public static implicit operator string(PositionCommand positionCommand)
+	{
+		var command = $"{UciConstants.POSITION_COMMAND} fen {positionCommand.Fen}";
+		if (positionCommand.Moves != null)
+		{
+			var movesPart = string.Join(' ', positionCommand.Moves);
+			if (!string.IsNullOrWhiteSpace(movesPart))
+				command += $" moves {movesPart}";
+		}
+
+		return command;
+	}
+
+	public override string ToString() => this;
+
+	public readonly void Deconstruct(out Fen fen, out IEnumerable<string>? moves)
+	{
+		fen   = Fen;
+		moves = Moves;
+	}
+}
