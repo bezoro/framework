777c9517856ce0a76f65b635c4617825c95078e1
Luiz Otávio Amaral Vaz <bezoro3dartist@outlook.com>
2025-08-22 09:28:19 -0300

    test(uci): add unit tests for UciEngineClient GoAsync behavior
    - Verify depth-based search executes with expected depth.
    - Ensure searches respect specified SearchMoves parameter.

---
+++ tests/Bezoro.UCI.Tests/UciEngineClientTests.cs
@@ -0,0 +1,38 @@
+using Bezoro.UCI.API.Types;
+using FluentAssertions;
+using JetBrains.Annotations;
+
+namespace Bezoro.UCI.Tests;
+
+[TestSubject(typeof(UciEngineClient))]
+public class UciEngineClientTests
+{
+	public const string STOCKFISH_PATH = "Engine/stockfish/stockfish-windows-x86-64-avx2.exe";
+
+	[Fact]
+	public async Task GoAsync_WithDepth_SearchesWithExpectedDepth()
+	{
+		var transport = new ProcessUciTransport(STOCKFISH_PATH);
+		var engine    = new UciEngineClient(transport);
+		await engine.StartAsync();
+
+		var searchResult = await engine.GoAsync(new() { Depth = 6 }, CancellationToken.None);
+
+		searchResult.Should().NotBeNull();
+		searchResult.ReachedDepth.Should().Be(6);
+	}
+
+	[Fact]
+	public async Task GoAsync_WithSearchMove_SearchesWithExpectedSearchMove()
+	{
+		var transport = new ProcessUciTransport(STOCKFISH_PATH);
+		var engine    = new UciEngineClient(transport);
+		await engine.StartAsync();
+		await engine.SetPositionAsync(Fen.Default, null, CancellationToken.None);
+
+		var searchResult = await engine.GoAsync(new() { SearchMoves = ["a2a4"] }, CancellationToken.None);
+
+		searchResult.Should().NotBeNull();
+		searchResult.PrincipalVariations.Count.Should().BeGreaterThan(0);
+	}
+}
