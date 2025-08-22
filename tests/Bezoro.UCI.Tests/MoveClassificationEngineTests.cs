c192a8533dfa7a2d4296a57f85e9dcff2643d018
Luiz Otávio Amaral Vaz <bezoro3dartist@outlook.com>
2025-08-22 12:37:10 -0300

    test(uci): add performance test for ClassifyAsync
    - Verify ClassifyAsync completes within 5000 ms for a default FEN position.

--- tests/Bezoro.UCI.Tests/MoveClassificationEngineTests.cs
+++ tests/Bezoro.UCI.Tests/MoveClassificationEngineTests.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using Bezoro.UCI.API.Types;
 using FluentAssertions;
 using JetBrains.Annotations;
@@ -8,6 +9,26 @@ namespace Bezoro.UCI.Tests;
 public class MoveClassificationEngineTests
 {
 	public const string STOCKFISH_PATH = "Engine/stockfish/stockfish-windows-x86-64-avx2.exe";
+
+	[Fact]
+	public async Task ClassifyAsync_Performance()
+	{
+		var fen   = Fen.Default;
+		var board = BoardState.FromFen(fen)!.Value;
+
+		await using var engine = new MoveClassificationEngine(STOCKFISH_PATH);
+		await engine.StartAsync();
+
+		var moveStream = engine.ClassifyAsync(fen, board);
+
+		moveStream.Should().NotBeNull();
+		var stopWatch = new Stopwatch();
+		stopWatch.Start();
+		await foreach (var move in moveStream) { }
+
+		stopWatch.Stop();
+		stopWatch.ElapsedMilliseconds.Should().BeLessThan(5000);
+	}
 
 	[Fact]
 	public async Task ClassifyAsync_WhenCalled_ReturnsClassifiedMovesStream()
