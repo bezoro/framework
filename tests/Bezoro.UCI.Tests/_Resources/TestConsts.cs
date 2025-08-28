ab0b1290095a333d89cbd2ed3a1ab5973464d141
Luiz Otávio Amaral Vaz <bezoro3dartist@outlook.com>
2025-08-28 15:33:38 -0300

    feat(tests): add constant for Stockfish engine path
    - Introduce `STOCKFISH_PATH` in `TestConsts`.

---
+++ tests/Bezoro.UCI.Tests/_Resources/TestConsts.cs
@@ -0,0 +1,6 @@
+namespace Bezoro.UCI.Tests._Resources;
+
+public static class TestConsts
+{
+	public const string STOCKFISH_PATH = "_Resources/Engine/stockfish/stockfish-windows-x86-64-avx2.exe";
+}
