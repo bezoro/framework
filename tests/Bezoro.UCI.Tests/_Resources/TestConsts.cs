12f4e6aa40f300585ede8632f0d2b57c3438a4a8
Luiz Otávio Amaral Vaz <bezoro3dartist@outlook.com>
2025-08-28 15:39:53 -0300

    fix(tests): correct `STOCKFISH_PATH` in `TestConsts`

--- tests/Bezoro.UCI.Tests/_Resources/TestConsts.cs
+++ tests/Bezoro.UCI.Tests/_Resources/TestConsts.cs
@@ -2,5 +2,5 @@ namespace Bezoro.UCI.Tests._Resources;
 
 public static class TestConsts
 {
-	public const string STOCKFISH_PATH = "_Resources/Engine/stockfish/stockfish-windows-x86-64-avx2.exe";
+	public const string STOCKFISH_PATH = "../../../_Resources/Engine/stockfish/stockfish-windows-x86-64-avx2.exe";
 }
