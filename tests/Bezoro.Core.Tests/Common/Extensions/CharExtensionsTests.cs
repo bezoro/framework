ff659042e833e7da92f5c530c700306f227a9886
Luiz Otávio Amaral Vaz <bezoro3dartist@outlook.com>
2025-08-29 16:08:17 -0300

    tests(core): add unit tests for CharExtensions
    - Introduce tests for `IsEmpty` and `ThrowIfEmpty` methods to validate behavior for whitespace and non-whitespace characters.
    - Add exception handling tests for `ThrowIfLetter`, `ThrowIfNumber`, `ThrowIfSymbol`, `ThrowIfUpperCase`, and `ThrowIfLowerCase` methods.
    - Ensure all test validations utilize `FluentAssertions` for consistency.

---
+++ tests/Bezoro.Core.Tests/Common/Extensions/CharExtensionsTests.cs
@@ -0,0 +1,129 @@
+using System;
+using Bezoro.Core.Common.Extensions;
+using FluentAssertions;
+using JetBrains.Annotations;
+using Xunit;
+
+namespace Bezoro.Core.Tests.Common.Extensions;
+
+[TestSubject(typeof(CharExtensions))]
+public static class CharExtensionsTests
+{
+	public class Unit
+	{
+		[Fact]
+		public void IsEmpty_WhenNotWhiteSpace_ReturnsFalse()
+		{
+			'a'.IsEmpty().Should().BeFalse();
+			'1'.IsEmpty().Should().BeFalse();
+			'#'.IsEmpty().Should().BeFalse();
+		}
+
+		[Fact]
+		public void IsEmpty_WhenWhiteSpace_ReturnsTrue()
+		{
+			' '.IsEmpty().Should().BeTrue();
+			'\t'.IsEmpty().Should().BeTrue();
+			'\n'.IsEmpty().Should().BeTrue();
+		}
+
+		[Fact]
+		public void ThrowIfEmpty_WhenNotWhiteSpace_ReturnsChar()
+		{
+			'a'.ThrowIfEmpty().Should().Be('a');
+			'1'.ThrowIfEmpty().Should().Be('1');
+			'#'.ThrowIfEmpty().Should().Be('#');
+		}
+
+		[Fact]
+		public void ThrowIfEmpty_WhenWhiteSpace_ThrowsArgumentException()
+		{
+			var act1 = () => ' '.ThrowIfEmpty();
+			var act2 = () => '\t'.ThrowIfEmpty();
+			var act3 = () => '\n'.ThrowIfEmpty();
+
+			act1.Should().Throw<ArgumentException>();
+			act2.Should().Throw<ArgumentException>();
+			act3.Should().Throw<ArgumentException>();
+		}
+
+		[Fact]
+		public void ThrowIfLetter_WhenLetter_ThrowsArgumentException()
+		{
+			var act1 = () => 'a'.ThrowIfLetter();
+			var act2 = () => 'Z'.ThrowIfLetter();
+
+			act1.Should().Throw<ArgumentException>();
+			act2.Should().Throw<ArgumentException>();
+		}
+
+		[Fact]
+		public void ThrowIfLetter_WhenNotLetter_ReturnsChar()
+		{
+			'1'.ThrowIfLetter().Should().Be('1');
+			'#'.ThrowIfLetter().Should().Be('#');
+		}
+
+		[Fact]
+		public void ThrowIfLowerCase_WhenLowerCase_ThrowsArgumentException()
+		{
+			var act = () => 'a'.ThrowIfLowerCase();
+
+			act.Should().Throw<ArgumentException>();
+		}
+
+		[Fact]
+		public void ThrowIfLowerCase_WhenNotLowerCase_ReturnsChar()
+		{
+			'A'.ThrowIfLowerCase().Should().Be('A');
+			'1'.ThrowIfLowerCase().Should().Be('1');
+			'#'.ThrowIfLowerCase().Should().Be('#');
+		}
+
+		[Fact]
+		public void ThrowIfNumber_WhenNotNumber_ReturnsChar()
+		{
+			'a'.ThrowIfNumber().Should().Be('a');
+			'#'.ThrowIfNumber().Should().Be('#');
+		}
+
+		[Fact]
+		public void ThrowIfNumber_WhenNumber_ThrowsArgumentException()
+		{
+			var act = () => '1'.ThrowIfNumber();
+
+			act.Should().Throw<ArgumentException>();
+		}
+
+		[Fact]
+		public void ThrowIfSymbol_WhenNotSymbol_ReturnsChar()
+		{
+			'a'.ThrowIfSymbol().Should().Be('a');
+			'1'.ThrowIfSymbol().Should().Be('1');
+		}
+
+		[Fact]
+		public void ThrowIfSymbol_WhenSymbol_ThrowsArgumentException()
+		{
+			var act = () => '©'.ThrowIfSymbol();
+
+			act.Should().Throw<ArgumentException>();
+		}
+
+		[Fact]
+		public void ThrowIfUpperCase_WhenNotUpperCase_ReturnsChar()
+		{
+			'a'.ThrowIfUpperCase().Should().Be('a');
+			'1'.ThrowIfUpperCase().Should().Be('1');
+			'#'.ThrowIfUpperCase().Should().Be('#');
+		}
+
+		[Fact]
+		public void ThrowIfUpperCase_WhenUpperCase_ThrowsArgumentException()
+		{
+			var act = () => 'A'.ThrowIfUpperCase();
+
+			act.Should().Throw<ArgumentException>();
+		}
+	}
+}
