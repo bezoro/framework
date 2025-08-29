bf756920a5babfcfd13e92a0d7d2e82928c9cad1
Luiz Otávio Amaral Vaz <bezoro3dartist@outlook.com>
2025-08-29 15:19:35 -0300

    tests(core): add unit tests for CollectionModify extension
    - Introduce `AddRange` method tests to validate appending multiple items to a collection.
    - Use `FluentAssertions` for improved test readability and robustness.

---
+++ tests/Bezoro.Core.Tests/Common/Extensions/Collections/Modify/CollectionModifyTests.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using Bezoro.Core.Common.Extensions.Collections.Modify;
+using FluentAssertions;
+using JetBrains.Annotations;
+using Xunit;
+
+namespace Bezoro.Core.Tests.Common.Extensions.Collections.Modify;
+
+[TestSubject(typeof(CollectionModify))]
+public class CollectionModifyTests
+{
+	[Fact]
+	public void AddRange_WhenValidCollection_ShouldAddItemsToCollection()
+	{
+		// Arrange
+		ICollection<int> sourceCollection = new List<int> { 1, 2, 3 };
+		ICollection<int> itemsToAdd       = new List<int> { 4, 5, 6 };
+
+		// Act
+		sourceCollection.AddRange(itemsToAdd);
+
+		// Assert
+		sourceCollection.Should().HaveCount(6);
+		sourceCollection.Should().ContainInOrder(1, 2, 3, 4, 5, 6);
+	}
+}
