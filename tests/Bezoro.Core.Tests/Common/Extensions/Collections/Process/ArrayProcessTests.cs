9b44feccb2981b79e64163575a4b0a304956db29
Luiz Otávio Amaral Vaz <bezoro3dartist@outlook.com>
2025-08-29 15:58:37 -0300

    tests(core): add unit tests for ArrayProcess extension
    - Introduce tests for `ProcessArray` and `ProcessArrayAsync` methods.
    - Validate handling of null arrays and elements using `ArgumentNullException`.
    - Add tests for processing `IProcessable` elements.
    - Ensure all test validations utilize `FluentAssertions`.

--- tests/Bezoro.Core.Tests/Common/Extensions/Collections/Process/ArrayProcessTests.cs
+++ tests/Bezoro.Core.Tests/Common/Extensions/Collections/Process/ArrayProcessTests.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Bezoro.Core.Common.Extensions.Collections.Process;
+using Bezoro.Core.Common.Interfaces;
 using FluentAssertions;
 using JetBrains.Annotations;
 using Xunit;
@@ -66,13 +68,113 @@ public static class ArrayProcessTests
 			await array.Process(
 				(element, _, _, _) =>
 				{
-					processedElements.Add(element);
+					processedElements.Add(element!);
 					return Task.CompletedTask;
 				},
 				CancellationToken.None);
 
 			// Assert
 			processedElements.Should().BeEquivalentTo("1", "3", "4");
+		}
+
+		[Fact]
+		public async Task ProcessArrayAsync_Should_ProcessAllNonNullElements()
+		{
+			// Arrange
+			string?[] array             = ["1", null, "3", "4"];
+			var       processedElements = new List<string>();
+
+			// Act
+			await array.ProcessArrayAsync(element =>
+			{
+				processedElements.Add(element);
+				return Task.CompletedTask;
+			});
+
+			// Assert
+			processedElements.Should().BeEquivalentTo("1", "3", "4");
+		}
+
+		[Fact]
+		public async Task ProcessArrayAsync_Should_ThrowArgumentNullException_WhenArrayIsNull()
+		{
+			// Arrange
+			string?[] array = null!;
+
+			// Act
+			var act = () => array.ProcessArrayAsync(_ => Task.CompletedTask);
+
+			// Assert
+			await act.Should().ThrowAsync<ArgumentNullException>();
+		}
+
+		[Fact]
+		public void ProcessArray_Should_ProcessAllNonNullElements()
+		{
+			// Arrange
+			string?[] array             = ["1", null, "3", "4"];
+			var       processedElements = new List<string>();
+
+			// Act
+			array.ProcessArray(element => processedElements.Add(element));
+
+			// Assert
+			processedElements.Should().BeEquivalentTo("1", "3", "4");
+		}
+
+		[Fact]
+		public void ProcessArray_Should_ProcessAllNonNullElements_WithIProcessable()
+		{
+			// Arrange
+			var items = new[]
+			{
+				new ProcessableItem(),
+				null,
+				new ProcessableItem()
+			};
+
+			// Act
+			items.ProcessArray();
+
+			// Assert
+			items[0]!.WasProcessed.Should().BeTrue();
+			items[2]!.WasProcessed.Should().BeTrue();
+		}
+
+		[Fact]
+		public void ProcessArray_Should_ThrowArgumentNullException_WhenArrayIsNull()
+		{
+			// Arrange
+			string?[] array = null!;
+
+			// Act
+			var act = () => array.ProcessArray(_ => { });
+
+			// Assert
+			act.Should().Throw<ArgumentNullException>();
+		}
+
+		[Fact]
+		public void ProcessArray_Should_ThrowArgumentNullException_WhenArrayIsNull_WithIProcessable()
+		{
+			// Arrange
+			ProcessableItem?[] array = null!;
+
+			// Act
+			var act = () => array.ProcessArray();
+
+			// Assert
+			act.Should().Throw<ArgumentNullException>();
+		}
+	}
+
+	private class ProcessableItem : IProcessable
+	{
+		public bool WasProcessed { get; private set; }
+
+		public void Process()
+		{
+			WasProcessed = true;
 		}
 	}
 }
