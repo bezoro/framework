d0d6bbe1a54669e141d8aa23275855676d079d31
Luiz Otávio Amaral Vaz <bezoro3dartist@outlook.com>
2025-08-30 09:39:39 -0300

    refactor(core): rename and restructure ArrayConversionExtensions
    - Rename class from `ArrayConvertionExtensions` to `ArrayConversionExtensions` to fix typo.
    - Move class to `Extensions.Collections.Conversion` namespace for improved organization.

--- src/Bezoro.Core/Common/Extensions/Collections/Conversion/ArrayConversionExtensions.cs
+++ src/Bezoro.Core/Common/Extensions/Collections/Conversion/ArrayConversionExtensions.cs
@@ -1,8 +1,8 @@
 using Bezoro.Core.Common.Helpers;
 
-namespace Bezoro.Core.Common.Extensions;
+namespace Bezoro.Core.Common.Extensions.Collections.Conversion;
 
-public static class ArrayConvertionExtensions
+public static class ArrayConversionExtensions
 {
 	public static T[] To1D<T>(this T[,] from) =>
 		ArrayConverter.From2Dto1D(from);
