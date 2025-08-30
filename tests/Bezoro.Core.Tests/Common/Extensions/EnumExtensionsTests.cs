5f07bba5de2d1b50fac200f46b7ab824f97788ba
Luiz Otávio Amaral Vaz <bezoro3dartist@outlook.com>
2025-08-30 12:12:48 -0300

    tests(core): add unit tests for EnumExtensions
    - Add tests for `IsDefined` method to validate behavior with both defined and undefined enum values.
    - Ensure test reliability using `FluentAssertions` and clear test cases.

---
+++ tests/Bezoro.Core.Tests/Common/Extensions/EnumExtensionsTests.cs
@@ -0,0 +1,45 @@
+using Bezoro.Core.Common.Extensions;
+using FluentAssertions;
+using JetBrains.Annotations;
+using Xunit;
+
+namespace Bezoro.Core.Tests.Common.Extensions;
+
+[TestSubject(typeof(EnumExtensions))]
+public static class EnumExtensionsTests
+{
+	public class Unit
+	{
+		[Fact]
+		public void IsDefined_ShouldReturnFalse_WhenValueIsNotDefined()
+		{
+			// Arrange
+			var value = (TestEnum)999;
+
+			// Act
+			bool result = value.IsDefined();
+
+			// Assert
+			result.Should().BeFalse();
+		}
+
+		[Fact]
+		public void IsDefined_ShouldReturnTrue_WhenValueIsDefined()
+		{
+			// Arrange
+			var value = TestEnum.First;
+
+			// Act
+			bool result = value.IsDefined();
+
+			// Assert
+			result.Should().BeTrue();
+		}
+	}
+
+	private enum TestEnum
+	{
+		First,
+		Second
+	}
+}
